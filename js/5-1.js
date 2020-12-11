88b426b9cc8b3d71658e0acb5a498ef4aab1bb08
Dmytro <mityaua@gmail.com>
2020-12-11 12:29:09 +0200

    Инициализация задач 5го блока

---
+++ js/5-1.js
@@ -0,0 +1,15 @@
+const parent = {
+  name: 'Stacey',
+  surname: 'Moore',
+  age: 54,
+  heritage: 'Irish',
+};
+// Пиши код ниже этой строки
+
+const child = {};
+
+// Пиши код выше этой строки
+child.name = 'Jason';
+child.age = 27;
+
+// Измени код так, чтобы объект parent стал прототипом для объекта в переменной сhild.
